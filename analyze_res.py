c110d04782f5dede549e9ecadb7672f0904238c1
skyline-nju <duanyu.nju@hotmail.com>
2017-03-08 23:16:24 +0800

    improve plot_phi

--- analyze_res.py
+++ analyze_res.py
@@ -38,7 +38,10 @@ def plot_peak(eta, eps):
                           bf["count_rhox"])
 
 
-def get_dict_sum(seg_nb: np.ndarray, seg_len: np.ndarray, seg_phi: np.ndarray):
+def get_dict_sum(seg_nb: np.ndarray,
+                 seg_len: np.ndarray,
+                 seg_phi: np.ndarray,
+                 dict0: dict):
     """ Transform array of phi into dict type with key nb.
 
         Parameters:
@@ -49,19 +52,18 @@ def get_dict_sum(seg_nb: np.ndarray, seg_len: np.ndarray, seg_phi: np.ndarray):
                 Length of each segment of time serials.
             seg_phi: np.ndarray
                 Mean order parameter of each segment.
-
-        Returns:
-        --------
-            Dict: dict
-                Key = nb, value is also a dict with key sum_phi and count.
+            dict0: dict
+                Dict as input and output
     """
-    n_set = np.unique(seg_nb)
-    dict_nb = {nb: {"sum_phi": 0, "count": 0} for nb in n_set if nb > 0}
     for i, nb in enumerate(seg_nb):
         if nb > 0:
-            dict_nb[nb]["sum_phi"] += seg_len[i] * seg_phi[i]
-            dict_nb[nb]["count"] += seg_len[i]
-    return dict_nb
+            sum_phi = seg_len[i] * seg_phi[i]
+            count = seg_len[i]
+            if nb in dict0:
+                dict0[nb]["sum_phi"] += sum_phi
+                dict0[nb]["count_phi"] += count
+            else:
+                dict0[nb] = {"sum_phi": sum_phi, "count_phi": count}
 
 
 def get_dict_mean(dict0: dict, layer: int) -> dict:
@@ -70,9 +72,9 @@ def get_dict_mean(dict0: dict, layer: int) -> dict:
         Parameters:
         --------
             dict0: dict
-                Sum of phi and count, with key seed or nb.
+                Dually or triply nested
             level: int
-                Layer of dict.
+                Layer of dict0.
         Returns:
         --------
             dict1: dict
@@ -80,11 +82,12 @@ def get_dict_mean(dict0: dict, layer: int) -> dict:
     """
     dict1 = {}
     if layer == 2:
-        tot = sum([dict0[nb]["count"] for nb in dict0])
+        tot = sum([dict0[nb]["count_phi"] for nb in dict0])
         for nb in dict0:
             dict1[nb] = {nb: {"mean_phi": 0, "rate_phi": 0}}
-            dict1[nb]["mean_phi"] = dict0[nb]["sum_phi"] / dict0[nb]["count"]
-            dict1[nb]["rate_phi"] = dict0[nb]["count"] / tot
+            dict1[nb]["mean_phi"] = dict0[nb]["sum_phi"] / dict0[nb][
+                "count_phi"]
+            dict1[nb]["rate_phi"] = dict0[nb]["count_phi"] / tot
     elif layer == 3:
         tot = 0  # key = nb
         dict1 = {}  # key = nb
@@ -92,30 +95,134 @@ def get_dict_mean(dict0: dict, layer: int) -> dict:
             for nb in dict0[seed]:
                 if nb in tot:
                     dict1[nb]["mean_phi"] += dict0[seed][nb]["sum_phi"]
-                    dict1[nb]["rate_phi"] += dict0[seed][nb]["count"]
+                    dict1[nb]["rate_phi"] += dict0[seed][nb]["count_phi"]
                 else:
                     dict1[nb]["mean_phi"] = dict0[seed][nb]["sum_phi"]
-                    dict1[nb]["rate_phi"] = dict0[seed][nb]["count"]
-                tot += dict0[seed][nb]["count"]
+                    dict1[nb]["rate_phi"] = dict0[seed][nb]["count_phi"]
+                tot += dict0[seed][nb]["count_phi"]
         for nb in tot:
             dict1[nb]["mean_phi"] /= tot
             dict1[nb]["rate_phi"] /= tot
     return dict1
 
 
-def plot_phi(eta, eps):
+def sum_over_time(eta: float, eps: float) -> dict:
+    """ sum phi over time for each nb, respectively."""
     files = glob.glob("mb_%d.%d.*.npz" % (eta, eps))
+    res = {}
     for file in files:
         bf = np.load(file)
         para = mb.get_para(file)
         Lx = para[2]
-        dict_sum = get_dict_sum(
-            bf["seg_num"], bf["seg_idx1"] - bf["seg_idx0"], bf["seg_phi"])
-        dict_mean = get_dict_mean(dict_sum, 2)
-        for nb in dict_mean:
-            rate = dict_mean[nb]["rate_phi"]
-            if rate > 0.2:
-                plt.scatter(Lx, dict_mean[nb]["mean_phi"], s=4, c=rate)
+        seed = para[4]
+        if Lx not in res:
+            res[Lx] = {seed: {}}
+        elif Lx in res and seed not in res[Lx]:
+            res[Lx][seed] = {}
+        get_dict_sum(bf["seg_num"], bf["seg_idx1"] - bf["seg_idx0"],
+                     bf["seg_phi"], res[Lx][seed])
+        for i, nb in enumerate(bf["num_set"]):
+            if nb > 0:
+                res[Lx][seed][nb]["sum_rhox"] = bf["sum_rhox"][i]
+                res[Lx][seed][nb]["count_rhox"] = bf["count_rhox"][i]
+    return res
+
+
+def time_ave(dict0: dict) -> dict:
+    """ Time average for phi and peak.
+
+        Parameters:
+        --------
+            dict0: dict
+                Return of fun: sum_over_time
+
+        Returns:
+        --------
+            res: dict
+                Time-averaged phi, rho_x and coresponding rate.
+    """
+    res = {}
+    for Lx in dict0:
+        res[Lx] = {}
+        for seed in dict0[Lx]:
+            res[Lx][seed] = {}
+            tot = 0
+            for nb in dict0[Lx][seed]:
+                res[Lx][seed][nb] = {}
+                res[Lx][seed][nb]["mean_phi"] = dict0[Lx][seed][nb][
+                    "sum_phi"] / dict0[Lx][seed][nb]["count_phi"]
+                res[Lx][seed][nb]["mean_rhox"] = dict0[Lx][seed][nb][
+                    "sum_rhox"] / dict0[Lx][seed][nb]["count_rhox"]
+                tot += dict0[Lx][seed][nb]["count_phi"]
+            for nb in dict0[Lx][seed]:
+                res[Lx][seed][nb]["rate"] = dict0[Lx][seed][nb][
+                    "count_phi"] / tot
+    return res
+
+
+def sample_ave(dict0: dict) -> dict:
+    """ Sample average for phi and peak.
+
+        Parameters:
+        --------
+            dict0: dict
+                Return of fun: sum_over_time
+
+        Returns:
+        --------
+            res: dict
+                Sample-averaged phi, rho_x and coresponding rate.
+    """
+    res = {}
+    for Lx in dict0:
+        res[Lx] = {}
+        tot = 0
+        for seed in dict0[Lx]:
+            for nb in dict0[Lx][seed]:
+                if nb in res[Lx]:
+                    res[Lx][nb]["sum_phi"] += dict0[Lx][seed][nb]["sum_phi"]
+                    res[Lx][nb]["count_phi"] += dict0[Lx][seed][nb][
+                        "count_phi"]
+                    res[Lx][nb]["sum_rhox"] += dict0[Lx][seed][nb]["sum_rhox"]
+                    res[Lx][nb]["count_rhox"] += dict0[Lx][seed][nb][
+                        "count_rhox"]
+                else:
+                    res[Lx][nb] = {}
+                    res[Lx][nb]["sum_phi"] = dict0[Lx][seed][nb]["sum_phi"]
+                    res[Lx][nb]["count_phi"] = dict0[Lx][seed][nb]["count_phi"]
+                    res[Lx][nb]["sum_rhox"] = dict0[Lx][seed][nb]["sum_rhox"]
+                    res[Lx][nb]["count_rhox"] = dict0[Lx][seed][nb][
+                        "count_rhox"]
+                tot += dict0[Lx][seed][nb]["count_phi"]
+        for nb in res[Lx]:
+            res[Lx][nb]["mean_phi"] = res[Lx][nb]["sum_phi"] / res[Lx][nb][
+                "count_phi"]
+            res[Lx][nb]["rate"] = res[Lx][nb]["count_phi"] / tot
+            res[Lx][nb]["mean_rhox"] = res[Lx][nb]["sum_rhox"] / res[Lx][nb][
+                "count_rhox"]
+    return res
+
+
+def plot_phi(eta, eps, SampleAve=False, rate_m=0.2):
+    sum_t = sum_over_time(eta, eps)
+
+    if SampleAve is False:
+        ave_t = time_ave(sum_t)
+        for Lx in ave_t:
+            for seed in ave_t[Lx]:
+                for nb in ave_t[Lx][seed]:
+                    rate = ave_t[Lx][seed][nb]["rate"]
+                    if rate > rate_m:
+                        phi = ave_t[Lx][seed][nb]["mean_phi"]
+                        plt.scatter(Lx, phi, s=4, c=rate)
+    else:
+        ave_s = sample_ave(sum_t)
+        for Lx in ave_s:
+            for nb in ave_s[Lx]:
+                rate = ave_s[Lx][nb]["rate"]
+                if rate > rate_m:
+                    phi = ave_s[Lx][nb]["mean_phi"]
+                    plt.scatter(Lx/(nb*200)-1, phi, s=4, c=rate)
     plt.colorbar()
     plt.show()
     plt.close()
@@ -141,4 +248,4 @@ if __name__ == "__main__":
     # para = mb.get_para(file)
     # mb.plot_rhox_mean(para, buff["num_set"], buff["sum_rhox"],
     #                   buff["count_rhox"])
-    plot_phi(350, 0)
+    plot_phi(350, 20, True, 0.3)
