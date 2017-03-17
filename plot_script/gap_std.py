625d9d7b1cfea571dda19adbab6b639760e05da6
skyline-nju <duanyu.nju@hotmail.com>
2017-03-17 23:57:22 +0800

    create gap_std.py

--- plot_script/gap_std.py
+++ plot_script/gap_std.py
@@ -1,18 +1,35 @@
 """ Plot standard deviation of gaps between two nearest bands."""
 
 import numpy as np
-import sys
 import os
 import matplotlib.pyplot as plt
-sys.path.append("../")
+from read_npz import fixed_para
 
-try:
-    import ana_data
-except:
-    raise
+
+def plot_phi_vs_std_gap(Lx, nb, marker, eta=350, eps=20, ax=None):
+    flag_show = False
+    if ax is None:
+        ax = plt.subplot(111)
+        flag_show = True
+
+    f = fixed_para("mean_phi", "std_gap", Lx=Lx, nb=nb)
+    for phi, std_gap in f:
+        plt.plot(std_gap/Lx, phi, marker, label=r"$L_x=%d, n_b=%d$" % (Lx, nb))
+
+    if flag_show:
+        plt.show()
+        plt.close()
 
 
 if __name__ == "__main__":
     os.chdir("E:\\data\\random_torque\\bands\\Lx\\snapshot\\uniband")
 
-    print("hello, world")
+    Lxs = [400, 420, 440, 460, 480]
+    mk = ["ko", "rs", "g>", "b<", "yp"]
+    ax = plt.subplot(111)
+    for i, Lx in enumerate(Lxs):
+        plot_phi_vs_std_gap(Lx, 2, mk[i], ax=ax)
+    # plt.legend(loc="best")
+    plt.show()
+    plt.close()
+
