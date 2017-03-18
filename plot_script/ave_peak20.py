f534cc9568f8f7b7bce8c94d4a52ad0f6df03397
skyline-nju <duanyu.nju@hotmail.com>
2017-03-18 19:19:02 +0800

    add func plot_rho_gas_vs_phi

--- plot_script/ave_peak20.py
+++ plot_script/ave_peak20.py
@@ -7,20 +7,56 @@ from axes_zoom_effect import zoom_effect03
 from read_npz import read_matched_file, eq_Lx_and_nb
 
 
+def plot_rho_gas_vs_phi(Lx, nb, eta=350, eps=20):
+    def one_Lx(Lx0, nb0, maker):
+        if Lx0 == 440:
+            os.chdir(path2)
+        else:
+            os.chdir(path1)
+        rho_0 = 1
+        dictLSN = read_matched_file({"Lx": Lx0, "eps": eps, "eta": eta})
+        f = eq_Lx_and_nb(Lx0, nb0, "mean_phi", dictLSN=dictLSN)
+        phi = np.array([i for i in f])
+        f = eq_Lx_and_nb(Lx0, nb0, "ave_peak", dictLSN=dictLSN)
+        rho_gas = np.array([peak[190:230].mean() for peak in f])
+        ax.plot(
+            rho_0 - rho_gas,
+            phi,
+            maker,
+            label=r"$L_x=%d, n_b=%d$" % (Lx0, nb0))
+        z = np.polyfit(rho_0 - rho_gas, phi, 1)
+        print(z)
+
+    path1 = "E:\\data\\random_torque\\bands\\Lx\\snapshot\\eps20"
+    path2 = "E:\\data\\random_torque\\bands\\Lx\\snapshot\\uniband"
+
+    ax = plt.subplot(111)
+    mk = ["ro", "bs", "g>", "k<"]
+    if isinstance(Lx, list):
+        for i, Lx0 in enumerate(Lx):
+            one_Lx(Lx0, nb[i], mk[i])
+    else:
+        one_Lx(Lx, nb)
+
+    plt.legend(loc="best")
+    plt.show()
+    plt.close()
+
+
 def plot_peak(Lx, nb, eta=350, eps=20):
     """ Plot time-averaged peaks for differernt samples with zoom effect."""
 
     os.chdir("E:\\data\\random_torque\\bands\\Lx\\snapshot\\uniband")
 
-    fig = plt.figure(1, figsize=(7, 6), dpi=200)
+    fig = plt.figure(1, figsize=(7, 6))
     ax1 = plt.subplot(211)
     ax2 = plt.subplot(223)
     ax3 = plt.subplot(224)
 
-    ax1.set_xlim(80, 200)
-    ax3.set_xlim(188, 196)
+    ax1.set_xlim(60, 240)
+    ax3.set_xlim(190, 230)
     ax3.set_ylim(0.45, 0.5)
-    zoom_effect03(ax3, ax1, 188, 196, loc="downward")
+    zoom_effect03(ax3, ax1, 190, 230, loc="downward")
 
     dictLSN = read_matched_file({"Lx": Lx, "eta": eta, "eps": eps})
     phi = np.array(
@@ -30,12 +66,15 @@ def plot_peak(Lx, nb, eta=350, eps=20):
 
     x = np.arange(Lx) + 0.5
     rho_gas = np.zeros_like(phi)
-    for i, peak in enumerate(eq_Lx_and_nb(Lx, nb, "ave_peak")):
+    for i, peak in enumerate(
+            eq_Lx_and_nb(Lx, nb, "ave_peak", dictLSN=dictLSN)):
         ax1.plot(x, peak, c=color_list[i], lw=0.8)
         ax3.plot(x, peak, c=color_list[i])
-        rho_gas[i] = np.mean(peak[190:195])
+        rho_gas[i] = np.mean(peak[190:230])
     ax2.axis("auto")
     sca = ax2.scatter(rho_gas, phi, c=phi, cmap="jet")
+    z = np.polyfit(rho_gas, phi, 1)
+    print(z)
 
     ax1.set_title(r"$(a)$")
     ax2.set_title(r"$(b)$")
@@ -55,3 +94,4 @@ def plot_peak(Lx, nb, eta=350, eps=20):
 
 if __name__ == "__main__":
     plot_peak(460, 2)
+    # plot_rho_gas_vs_phi([440, 660, 880], [2, 3, 4])
